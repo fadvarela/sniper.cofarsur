b360e0953fa36db24091611bdaf29146534a7c94
fadvarela <fvarela.sbot@gmail.com>
2020-05-11 16:04:16 -0300

    * Se quitan signo de interrogaciòn a modelo Novedades del backend. * Se quita opciòn Marcaciones del menù Novedades (del Home-Menu)

--- DataAccess/Models/RRHH/Novedad.cs
+++ DataAccess/Models/RRHH/Novedad.cs
@@ -8,13 +8,13 @@ namespace DataAccess.Models.RRHH
 		public string Seccion { get; set; }
 
 		[Column("ID_JORNADA")]
-		public long? IdJornada { get; set; }
+		public long IdJornada { get; set; }
 
 		[Column("N_JORNADA")]
 		public string Njornada { get; set; }
 
 		[Column("ID_INCIDENCIA")]
-		public long? IdIncidencia { get; set; }
+		public long IdIncidencia { get; set; }
 
 		[Column("N_INCIDENCIA")]
 		public string Nincidencia { get; set; }
@@ -29,10 +29,10 @@ namespace DataAccess.Models.RRHH
 		public decimal Htrabajadas { get; set; }
 
 		[Column("I_IMPARES")]
-		public long? Iimpares { get; set; }
+		public long Iimpares { get; set; }
 
 		[Column("I_SIN_INCIDENCIAS")]
-		public long? IsinIncidencias { get; set; }
+		public long IsinIncidencias { get; set; }
 
 		[Column("H_ADICIONALES")]
 		public decimal Hadicionales { get; set; }
