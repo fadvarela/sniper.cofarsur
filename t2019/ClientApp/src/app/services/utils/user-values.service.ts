4bd85eecd839f0f2643948a9d2e53cbc3216804c
Maxi <55333088+nachomaldo778@users.noreply.github.com>
2020-05-11 16:00:23 -0300

    Agregado tooltip para boton logout del menu

--- t2019/ClientApp/src/app/services/utils/user-values.service.ts
+++ t2019/ClientApp/src/app/services/utils/user-values.service.ts
@@ -15,6 +15,9 @@ export class UserValuesService {
   private usuarioLogueado: Usuario;
   private loggedInStatus = false;
   private status = false;
+
+  /* Objeto para observar la inactividad del usuario.
+  si no presiona nada por 15 minutos, lo deslogueamos */
   idle = new Idle()
     .whenNotInteractive()
     .within(15)
@@ -38,10 +41,16 @@ export class UserValuesService {
     }
   }
 
+  /* Devolvemos una promesa para asi hacer logica en
+    el componente.
+  */
   setLogin(userName: string, pass: string): Promise<boolean> {
     return this.loadLoginUser(userName, pass).then(() => this.usuarioLogueado.Ok);
   }
 
+  /* Valida contra la BD con usr y pass. Si obtenemos un TRUE como respuesta
+    guardamos todos los valores necesarios del usuario en las cookies del browser
+  */
   loadLoginUser(userName: string, pass: string) {
     return this.loginService.ValidarUsuario(userName, pass).toPromise().then((result: Usuario) => {
       this.usuarioLogueado = result;
@@ -51,9 +60,13 @@ export class UserValuesService {
         this.setLoggedIn(true);
         this.route.navigate(['home']);
       }
-    }).catch((error) => { this.getUsuarioValues.Mensaje = error.error; this.getUsuarioValues.Ok = false;});
+    }).catch((error) => { this.getUsuarioValues.Mensaje = error.error; this.getUsuarioValues.Ok = false; });
   }
 
+  /* Si estamos en la pantalla de LOGIN, envia un TRUE para
+   crear un nuevo usuario logueado.
+   Si se refresca la pagina, busca los datos que esten
+   en las cookies y los obtenemos. */
   getUsuarioSuccess(nuevoLogin) {
     let usuarioCookie = new Usuario();
     if (nuevoLogin) {
@@ -71,14 +84,19 @@ export class UserValuesService {
     return usuarioCookie;
   }
 
+  // Setea el login en TRUE cuando inicia sesion
   setLoggedIn(value: boolean) {
     this.loggedInStatus = value;
   }
 
+  // Nos devuelve el estado del logueo (si esta logueado o no)
   get isLogueado() {
     return this.loggedInStatus;
   }
 
+  // Para desloguearnos. Cierra todos los modals que esten
+  // abiertos en la app. Luego llama a funcion para
+  // que esten almacenados en la web
   setLogout() {
     this.dialogRef.closeAll();
     this._snackBar.openSnackBar('snack-warning', 'Redirigiendo al login', 3000);
@@ -91,6 +109,9 @@ export class UserValuesService {
     });
   }
 
+  // Primero verifica que haya una cookie
+  // si existe, elimina todo dato almacenado en el navegador
+  // y devuelve un TRUE
   removeLogoutData(): Observable<boolean> {
     if (!this.checkCookieExists) {
       return of(false);
@@ -105,10 +126,13 @@ export class UserValuesService {
     return of(true);
   }
 
+  // Nos devuelve el estado de la cookie
+  // si existe o no
   get checkCookieExists() {
     return this.cookieService.check('logueado');
   }
 
+  // Nos devuelve el objeto del usuario logueado
   get getUsuarioValues() {
     return this.usuarioLogueado;
   }
